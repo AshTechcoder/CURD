d8e2552fe90cc716ea114f3ac90abce4898f7f99
AshTechcoder <ashtechcode.devop16@gmail.com>
2020-07-14 16:11:19 +0530

    About Activity is created and intent is changed to AboutActivity.class

--- app/src/main/java/com/example/curd/Activities/AboutActivity.java
+++ app/src/main/java/com/example/curd/Activities/AboutActivity.java
@@ -1,8 +1,13 @@
 package com.example.curd.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.curd.R;
 
@@ -12,5 +17,33 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ///Setting up toolbar
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+
+        //Drawable drawable = toolbar.getNavigationIcon();
+        //drawable.setColorFilter(ContextCompat.getColor(AboutActivity.this, R.color.colorAccentx));
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.colorAccent));
+        getSupportActionBar().setTitle("About");
+
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
 }
